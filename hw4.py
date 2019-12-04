f23ebf7fd807b53748d04710937d959db6f1c35b
Jeff Yang <www.speedway@hotmail.com>
2019-12-03 22:39:18 -0500

    草

--- hw4.py
+++ hw4.py
@@ -21,14 +21,21 @@ def run():
     k = int(sys.argv[3])
     my_hostname = socket.gethostname() # Gets my host name
     my_address = socket.gethostbyname(my_hostname) # Gets my IP address from my hostname
-
+    
+    #don't ask, gRPC tutorial said so
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    csci4220_hw4_pb2_grpc.add_KadImplServicer_to_server(KadImplServicer(), server)
+    
+    #listen from the port
+    server.add_insecure_port(my_address + ':' + my_port)
+    server.start()
     
 	# Use the following code to convert a hostname to an IP and start a channel Note that every stub needs a channel attached to it When you are done with a channel you should call .close() on the channel. Submitty may kill your program if you have too many file descriptors open at the same time.
 	
-    remote_addr = socket.gethostbyname(my_hostname)
-    remote_port = int(my_port)
+    #remote_addr = socket.gethostbyname(my_hostname)
+    #remote_port = int(my_port)
     
-    channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
+    #channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
     
     while True:
         input_str = str(raw_input())
@@ -38,17 +45,25 @@ def run():
             print("bootstrap")
             remote_hostname = input_args[1]
             remote_port = int(input_args[2])
+            remote_addr = socket.gethostbyname(remote_hostname)
+            
+            #connect to server
+            channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
+            
+            #FindNode(?)
             
         if input_args[0] == "QUIT":
             break
         
         
+#actual functions are implemented here, don't ask me why I created a class for it because
+#the gRPC tutorial said so
 class KadImplServicer(csci4220_hw4_pb2_grpc.KadImplServicer):
     def __init__(self):
         pass
     
     def FindNode(self, request, context):
-        
+        pass
         
 if __name__ == '__main__':
     run()
