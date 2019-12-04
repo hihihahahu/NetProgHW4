7ef6a585428dc55fd1a30fa4d257eadc09055ef7
Jeff Yang <www.speedway@hotmail.com>
2019-12-03 21:41:06 -0500

    update

--- hw4.py
+++ hw4.py
@@ -14,12 +14,15 @@ def run():
         print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
         sys.exit(-1)
 
+    bucket = []
+    
     local_id = int(sys.argv[1])
     my_port = str(int(sys.argv[2])) # add_insecure_port() will want a string
     k = int(sys.argv[3])
     my_hostname = socket.gethostname() # Gets my host name
     my_address = socket.gethostbyname(my_hostname) # Gets my IP address from my hostname
 
+    
 	# Use the following code to convert a hostname to an IP and start a channel Note that every stub needs a channel attached to it When you are done with a channel you should call .close() on the channel. Submitty may kill your program if you have too many file descriptors open at the same time.
 	
     remote_addr = socket.gethostbyname(my_hostname)
@@ -27,9 +30,25 @@ def run():
     
     channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
     
-    print(remote_addr)
-    print(remote_port)
-    print(channel)
- 
+    while True:
+        input_str = str(raw_input())
+        input_args = input_str.split()
+        
+        if input_args[0] == "BOOTSTRAP":
+            print("bootstrap")
+            remote_hostname = input_args[1]
+            remote_port = int(input_args[2])
+            
+        if input_args[0] == "QUIT":
+            break
+        
+        
+class KadImplServicer(csci4220_hw4_pb2_grpc.KadImplServicer):
+    def __init__(self):
+        pass
+    
+    def FindNode(self, request, context):
+        
+        
 if __name__ == '__main__':
     run()
